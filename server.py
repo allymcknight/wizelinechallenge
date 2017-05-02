1ed7a71e5befd218672f3081edf4aa8ddce63baf
MARY ELLEN TONG <maryellentong@MARYs-MacBook-Air.local>
2017-05-02 10:33:34 -0700

    added templates and functionality to get new url

--- server.py
+++ server.py
@@ -18,8 +18,7 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
-
+    return render_template('home.html')
 
 
 @app.route('/new_url', methods=['POST'])
@@ -29,9 +28,54 @@ def create_new_url():
     original_url = request.form.get('orig_url')
     custom_url = request.form.get('custom_url')
 
+    row = Url.query.filter_by(original_url=original_url).first()
+
+    if row:
+        
+        return row.shortened_url
+
+    if custom_url:
+        custom_row = Url.query.filter_by(shortened_url=custom_url).first()
+
+        if custom_row:
+
+            flash("We're sorry, that custom shortened url is already taken")
+            return redirect('/')
+        else:
+
+            new_shortened = Url(original_url=original_url, shortened_url=custom_url)
+            db.session.add(new_shortened)
+            db.session.commit()
+            return custom_url
+
+
+
+    new_shortened = Url(original_url=original_url)
+    db.session.add(new_shortened)
+
+    new_shortened.shortened_url = hex(int(new_shortened.id))
+    db.session.commit()
+
+    return new_shortened.shortened_url
+
+
+@app.route('/<short_url>')
+def return_original_route(short_url):
+    """Gets shortened url and returns original from DB"""
+
+    url_info = Url.query.filter_by(shortened_url=short_url).first()
+
+    if url_info:
+        return redirect(url_info.original_url)
+
+    else:
+        return render_template('not-here.html')
+
+
+
 
    
-    return redirect('/accomplishments/%s'% str(user.user_id))
+    return 
 
 
 @app.route('/all_urls')
